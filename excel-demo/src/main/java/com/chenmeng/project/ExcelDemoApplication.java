3bdfe758ba3123077e147b67660a5e8a8245e58c
chenmeng <2691357857@qq.com>
2023-11-09 18:13:47 +0800

    初始化

---
+++ excel-demo/src/main/java/com/chenmeng/project/ExcelDemoApplication.java
@@ -0,0 +1,18 @@
+package com.chenmeng.project;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wyy
+ */
+@SpringBootApplication
+@MapperScan("com.chenmeng.project.mapper")
+public class ExcelDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelDemoApplication.class, args);
+    }
+
+}
