a6c0144fbc07905d22a52df7a7f8dc4116dac407
chenmeng <2691357857@qq.com>
2023-12-15 16:27:28 +0800

    438, 560 -- 题解提交

---
+++ leetcode/src/main/java/hot_100/_438_找到字符串中所有字母异位词.java
@@ -0,0 +1,122 @@
+package hot_100;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 找到字符串中所有字母异位词 -- 哈希表, 字符串, 滑动窗口
+ *
+ * @author 沉梦听雨
+ **/
+@SuppressWarnings("all")
+public class _438_找到字符串中所有字母异位词 {
+
+    /* 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，
+       返回这些子串的起始索引。不考虑答案输出的顺序。
+       异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。 */
+
+    /**
+     * 1、滑动窗口 -- 38ms(26.77%), 42.73MB(62.58%)
+     * <p>
+     * 时间复杂度为 O(p + s), p 是字符串 p 的长度, s是字符串 s 的长度
+     * <p>
+     * 空间复杂度为 O(p), p 是字符串 p 的长度
+     */
+    class Solution {
+        public List<Integer> findAnagrams(String s, String p) {
+            List<Integer> result = new ArrayList<>();
+
+            // 存储目标字符串 p 中每个字符的出现次数
+            HashMap<Character, Integer> targetFreqMap = new HashMap<>();
+            for (char c : p.toCharArray()) {
+                targetFreqMap.put(c, targetFreqMap.getOrDefault(c, 0) + 1);
+            }
+
+            int pLen = p.length();
+            int left = 0, right = 0, count = pLen;
+
+            while (right < s.length()) {
+                char currentChar = s.charAt(right);
+
+                // 如果当前字符是目标字符串 p 中的字符，更新 count
+                if (targetFreqMap.containsKey(currentChar) && targetFreqMap.get(currentChar) > 0) {
+                    count--;
+                }
+
+                // 更新目标字符串中当前字符的出现次数
+                targetFreqMap.put(currentChar, targetFreqMap.getOrDefault(currentChar, 0) - 1);
+
+                // 移动右指针
+                right++;
+
+                // 当 count 等于 0 时，表示找到一个合法的字母异位词
+                if (count == 0) {
+                    result.add(left);
+                }
+
+                // 当窗口大小等于目标字符串长度时，左指针右移，恢复 count 和频率表
+                if (right - left == pLen) {
+                    char leftChar = s.charAt(left);
+
+                    // 恢复 count
+                    if (targetFreqMap.containsKey(leftChar) && targetFreqMap.get(leftChar) >= 0) {
+                        count++;
+                    }
+
+                    // 恢复频率表
+                    targetFreqMap.put(leftChar, targetFreqMap.getOrDefault(leftChar, 0) + 1);
+
+                    // 移动左指针
+                    left++;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    /**
+     * 2、滑动窗口优化版（双指针+数组） -- 4ms(98.83%), 42.32MB(97.00%)
+     * <p>
+     * 时间复杂度为 O(m + n), m 是字符串 s 的长度, n 是字符串 p 的长度
+     * <p>
+     * 空间复杂度为 O(C), C 是字符集大小, 即 C = 26
+     */
+    class Solution2 {
+        public List<Integer> findAnagrams(String s, String p) {
+            // 计算s和p的长度
+            int m = s.length(), n = p.length();
+            // 创建一个存储结果的列表
+            List<Integer> res = new ArrayList<>();
+            // 如果s的长度小于p的长度，则直接返回空列表
+            if (m < n) {
+                return res;
+            }
+            // 创建一个长度为26的数组，用来存储p中每个字符出现的次数
+            int[] cnt1 = new int[26];
+            // 遍历p，将每个字符出现的次数存入cnt1中
+            for (int i = 0; i < n; ++i) {
+                cnt1[p.charAt(i) - 'a']++;
+            }
+            // 创建一个长度为26的数组，用来存储s中每个字符出现的次数
+            int[] cnt2 = new int[26];
+            // 遍历s，将每个字符出现的次数存入cnt2中
+            for (int i = 0, j = 0; i < m; ++i) {
+                int k = s.charAt(i) - 'a';
+                cnt2[k]++;
+                // 如果cnt2中某个字符出现的次数大于cnt1中对应字符出现的次数，则将cnt2中对应字符出现的次数减1，并且j自增1
+                while (cnt2[k] > cnt1[k]) {
+                    cnt2[s.charAt(j++) - 'a']--;
+                }
+                // 如果i - j + 1等于n，则将j的值存入res中
+                if (i - j + 1 == n) {
+                    res.add(j);
+                }
+            }
+            // 返回res
+            return res;
+        }
+    }
+}
---
+++ leetcode/src/main/java/hot_100/_560_和为K的子数组.java
@@ -0,0 +1,84 @@
+package hot_100;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 和为 K 的子数组 -- 数组, 哈希表, 前缀和
+ *
+ * @author 沉梦听雨
+ **/
+@SuppressWarnings("all")
+public class _560_和为K的子数组 {
+
+    /* 给你一个整数数组 nums 和一个整数 k ，
+       请你统计并返回 该数组中和为 k 的子数组的个数 。
+       子数组是数组中元素的连续非空序列。 */
+
+    /**
+     * 1、暴力解法 -- 1606ms(31.36%), 44.44MB(53.38%)
+     * <p>
+     * 时间复杂度分析：O(n^2)，其中 n 是数组的长度。
+     * <p>
+     * 空间复杂度分析：O(1)。
+     */
+    public class Solution {
+        public int subarraySum(int[] nums, int k) {
+            int count = 0;
+            int len = nums.length;
+
+            for (int left = 0; left < len; left++) {
+                int sum = 0;
+                for (int right = left; right < len; right++) {
+                    sum += nums[right];
+                    if (sum == k) {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+
+    /**
+     * 2、前缀和 + 哈希表 -- 25ms(54.20%), 46.03MB(5.03%)
+     * <p>
+     * 时间复杂度：O(n)。
+     * <p>
+     * 空间复杂度为：O(n)。
+     */
+    class Solution2 {
+        public int subarraySum(int[] nums, int k) {
+            // 创建一个哈希表，用于存储前缀和及其出现的次数
+            Map<Integer, Integer> counter = new HashMap<>();
+            // 初始化哈希表，表示前缀和为 0 出现了 1 次
+            counter.put(0, 1);
+            // 初始化答案为 0，s 用于表示当前的前缀和
+            int ans = 0, s = 0;
+
+            // 遍历数组 nums 中的每个元素
+            for (int num : nums) {
+                // 计算当前的前缀和
+                s += num;
+
+                // 更新答案，如果存在前缀和为 (s - k) 的情况，累加对应次数到答案中
+                ans += counter.getOrDefault(s - k, 0);
+
+                // 更新哈希表，记录当前前缀和出现的次数
+                counter.put(s, counter.getOrDefault(s, 0) + 1);
+            }
+
+            // 返回最终答案
+            return ans;
+        }
+    }
+
+    // public static void main(String[] args) {
+    //     int[] nums = {1, 1, 1};
+    //     int k = 2;
+    //     System.out.println(new Solution2().subarraySum(nums, k));
+    // }
+}
+
