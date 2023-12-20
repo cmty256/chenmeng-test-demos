93d7e28ab22ea823ee6d5361313c3e83fdd757d2
chenmeng <2691357857@qq.com>
2023-12-20 15:59:42 +0800

    41, 238 -- 题解提交

---
+++ leetcode/src/main/java/hot_100/_238_除自身以外数组的乘积.java
@@ -0,0 +1,41 @@
+package hot_100;
+
+/**
+ * 除自身以外数组的乘积 -- 数组、前缀和
+ *
+ * @author 沉梦听雨
+ **/
+@SuppressWarnings("all")
+public class _238_除自身以外数组的乘积 {
+
+    /* 给你一个整数数组 nums，返回 数组 answer ，
+       其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+       题目数据 保证 数组 nums 之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+       请 不要使用除法，且在 O(n) 时间复杂度内完成此题。 */
+
+    /**
+     * 1、两次循环 -- 1ms(100.00%), 51.84MB(9.51%)
+     * <p>
+     * 时间复杂度为：O(n)。
+     * <p>
+     * 空间复杂度为：O(1), 忽略答案数组的空间消耗，额外空间复杂度为 O(1)。
+     */
+    class Solution {
+        public int[] productExceptSelf(int[] nums) {
+            int n = nums.length;
+            int[] ans = new int[n];
+
+            // 计算每个元素的前缀积（即下三角）
+            for (int i = 0, down = 1; i < n; i++) {
+                ans[i] = down;
+                down *= nums[i];
+            }
+            // 计算每个元素的后缀积（即上三角）
+            for (int i = n - 1, up = 1; i >= 0; i--) {
+                ans[i] *= up;
+                up *= nums[i];
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcode/src/main/java/hot_100/_41_缺失的第一个正数.java
@@ -0,0 +1,92 @@
+package hot_100;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * 缺失的第一个正数 -- 数组、哈希表
+ *
+ * @author 沉梦听雨
+ **/
+@SuppressWarnings("all")
+public class _41_缺失的第一个正数 {
+
+    /* 给你一个整数数组 nums，返回 数组 answer ，
+       其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+       题目数据 保证 数组 nums 之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+       请 不要使用除法，且在 O(n) 时间复杂度内完成此题。 */
+
+    /**
+     * 1、排序 + HashSet -- 20ms(5.5%), 57MB(87.37%)
+     * <p>
+     * 时间复杂度为：O(nlogn)。
+     * <p>
+     * 额外空间复杂度为：O(1)。
+     */
+    class Solution {
+        public int firstMissingPositive(int[] nums) {
+            // 创建一个HashSet
+            HashSet<Integer> set = new HashSet<>();
+            // 对数组进行排序
+            Arrays.sort(nums);
+            // 获取数组的长度
+            int n = nums.length;
+
+            // 将数组中的元素添加到HashSet中
+            for (int i = 0; i < n; i++) {
+                set.add(nums[i]);
+            }
+            // 遍历数组，查找第一个不存在的正数
+            for (int i = 1; i < nums[n - 1]; i++) {
+                // 如果HashSet中不存在该正数，则返回该正数
+                if (!set.contains(i)) {
+                    return i;
+                }
+            }
+
+            // 如果数组中的最后一个元素大于0，则返回 该元素加1，否则返回 1
+            return nums[n - 1] > 0 ? (nums[n - 1] + 1) : 1;
+        }
+    }
+
+    /**
+     * 2、原地转换 -- 1ms(5.5%), 53.59MB(98.38%)
+     * <p>
+     * 时间复杂度为：O(n)。
+     * <p>
+     * 额外空间复杂度为：O(1)。
+     */
+    class Solution2 {
+        public int firstMissingPositive(int[] nums) {
+            int n = nums.length;
+
+            // 遍历数组
+            for (int i = 0; i < n; ++i) {
+                // 将当前元素放到正确的位置上，即 nums[i] 应该等于 i + 1
+                // 当前元素应该在的位置是 nums[i] - 1，如果不在则进行交换
+                // (while)判断条件：当前元素是正整数，当前元素不超过数组的长度，当前元素应该在的位置的元素不等于当前元素
+                while (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                    swap(nums, i, nums[i] - 1);
+                }
+            }
+
+            // 再次遍历数组，寻找第一个不在正确位置上的元素（缺失的最小正整数一定在数组长度内或者数组长度+1）
+            for (int i = 0; i < n; ++i) {
+                if (i + 1 != nums[i]) {
+                    // 如果找到第一个不在正确位置上的元素，则返回缺失的最小正整数
+                    return i + 1;
+                }
+            }
+
+            // 如果数组中所有元素都在正确位置上，返回数组长度加1
+            return n + 1;
+        }
+
+        // 交换数组中两个元素的位置
+        private void swap(int[] nums, int i, int j) {
+            int t = nums[i];
+            nums[i] = nums[j];
+            nums[j] = t;
+        }
+    }
+}
