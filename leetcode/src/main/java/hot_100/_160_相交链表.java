a2be5285c4d13ff72fcd8363bed9222750c77af4
chenmeng <2691357857@qq.com>
2023-12-26 10:52:03 +0800

    160, 206 -- 题解提交

---
+++ leetcode/src/main/java/hot_100/_160_相交链表.java
@@ -0,0 +1,68 @@
+package hot_100;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 相交链表 -- 数组、二分查找、分治、矩阵
+ *
+ * @author 沉梦听雨
+ **/
+@SuppressWarnings("all")
+public class _160_相交链表 {
+
+    /* 给你两个单链表的头节点 headA 和 headB ，
+       请你找出并返回两个单链表相交的起始节点。
+       如果两个链表不存在相交节点，返回 null 。 */
+
+    /**
+     * 1、遍历 + HashSet -- 6ms(13.58%), 46.7MB(5.00%)
+     * <p>
+     * 时间复杂度：O(m + n), 其中 m 和 n 分别是 headA 和 headB 的长度。
+     * <p>
+     * 空间复杂度：O(m), HashSet 存储的是 headA 的所有节点
+     */
+    public class Solution {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            Set<ListNode> hashSet = new HashSet<>();
+            // 先遍历A链表,并把A链表的所有节点加入到哈希集合中
+            ListNode temp = headA;
+            while (temp != null) {
+                hashSet.add(temp);
+                temp = temp.next;
+            }
+            // 再遍历B链表
+            temp = headB;
+            while (temp != null) {
+                // 判断集合中是否包含B链表的此节点
+                if (hashSet.contains(temp)) {
+                    // 包含，返回相交节点
+                    return temp;
+                }
+                // 不包含，遍历下一个节点
+                temp = temp.next;
+            }
+            // 没有相交节点，返回null
+            return null;
+        }
+    }
+
+    /**
+     * 2、双指针 -- 1ms(99.09%), 47.42MB(5.00%)
+     * <p>
+     * 时间复杂度：O(m + n), 其中 m 和 n 分别是 headA 和 headB 的长度。
+     * <p>
+     * 空间复杂度：O(1)
+     */
+    public class Solution2 {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            ListNode a = headA, b = headB;
+            while (a != b) {
+                a = a == null ? headB : a.next;
+                b = b == null ? headA : b.next;
+            }
+            return a;
+        }
+    }
+
+}
---
+++ leetcode/src/main/java/hot_100/_206_反转链表.java
@@ -0,0 +1,71 @@
+package hot_100;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 相交链表 -- 数组、二分查找、分治、矩阵
+ *
+ * @author 沉梦听雨
+ **/
+@SuppressWarnings("all")
+public class _206_反转链表 {
+
+    /* 给你两个单链表的头节点 headA 和 headB ，
+       请你找出并返回两个单链表相交的起始节点。
+       如果两个链表不存在相交节点，返回 null 。 */
+
+    /**
+     * 1、迭代（双指针） -- 0ms(100.00%), 41.18MB(5.01%)
+     * <p>
+     * 时间复杂度：O(n), 其中 n 为链表的长度。
+     * <p>
+     * 空间复杂度：O(1)
+     */
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            // 假设最前面是个null
+            ListNode prev = null;
+            // 当前节点，初始值为头节点。
+            ListNode curr = head;
+
+            // 遍历链表，每遍历一次就转换一次箭头
+            while (curr != null) {
+                // 存储当前节点（头节点）的下一节点
+                ListNode next = curr.next;
+                // null（prev）->curr 变为 null（prev）<-curr，转换箭头
+                curr.next = prev;
+                // 前置指针后移一位
+                prev = curr;
+                // 后置指针后移一位
+                curr = next;
+            }
+            return prev;
+        }
+    }
+
+    /**
+     * 2、递归 -- 0ms(100.00%), 41.30MB(5.01%)
+     * <p>
+     * 时间复杂度：O(n), 其中 n 为链表的长度。
+     * <p>
+     * 空间复杂度：O(n)
+     */
+    class Solution2 {
+        public ListNode reverseList(ListNode head) {
+            // 空链表 或者 尾结点
+            if(head == null || head.next == null) {
+                return head;
+            }
+            // 递归
+            ListNode res = reverseList(head.next);
+            // 反转箭头
+            head.next.next = head;
+            // 每一节点指向null，把null放最后
+            head.next = null;
+
+            return res;
+        }
+    }
+
+}
