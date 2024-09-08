9797c79dae0153f8fad4a5b44db99af0d5d114fc
chenmeng <2691357857@qq.com>
2024-09-08 20:46:21 +0800

    开闭原则提交

--- design-patterns-demo/src/main/java/com/chenmeng/project/principle/ocp/Ocp.java
+++ design-patterns-demo/src/main/java/com/chenmeng/project/principle/ocp/Ocp.java
@@ -1,65 +1,68 @@
 package com.chenmeng.project.principle.ocp;
 
+/**
+ * 不符合开闭原创，每次新增一个图形都需要修改 [使用方] 代码
+ */
 public class Ocp {
 
-	public static void main(String[] args) {
-		//ʹ�ÿ������ڵ�����
-		GraphicEditor graphicEditor = new GraphicEditor();
-		graphicEditor.drawShape(new Rectangle());
-		graphicEditor.drawShape(new Circle());
-		graphicEditor.drawShape(new Triangle());
-	}
+    public static void main(String[] args) {
+        // 使用看看存在的问题
+        GraphicEditor graphicEditor = new GraphicEditor();
+        graphicEditor.drawShape(new Rectangle());
+        graphicEditor.drawShape(new Circle());
+        graphicEditor.drawShape(new Triangle());
+    }
 
 }
 
-//����һ�����ڻ�ͼ���� [ʹ�÷�]
+// 这是一个用于绘图的类 [使用方]
 class GraphicEditor {
-	//����Shape����Ȼ�����type�������Ʋ�ͬ��ͼ��
-	public void drawShape(Shape s) {
-		if (s.m_type == 1)
-			drawRectangle(s);
-		else if (s.m_type == 2)
-			drawCircle(s);
-		else if (s.m_type == 3)
-			drawTriangle(s);
-	}
+    // 接收Shape对象，然后根据type，来绘制不同的图形
+    public void drawShape(Shape s) {
+        if (s.m_type == 1)
+            drawRectangle(s);
+        else if (s.m_type == 2)
+            drawCircle(s);
+        else if (s.m_type == 3)
+            drawTriangle(s);
+    }
 
-	//���ƾ���
-	public void drawRectangle(Shape r) {
-		System.out.println(" ���ƾ��� ");
-	}
+    // 绘制矩形
+    public void drawRectangle(Shape r) {
+        System.out.println(" 绘制矩形 ");
+    }
 
-	//����Բ��
-	public void drawCircle(Shape r) {
-		System.out.println(" ����Բ�� ");
-	}
-	
-	//����������
-	public void drawTriangle(Shape r) {
-		System.out.println(" ���������� ");
-	}
+    // 绘制圆形
+    public void drawCircle(Shape r) {
+        System.out.println(" 绘制圆形 ");
+    }
+
+    // 绘制三角形
+    public void drawTriangle(Shape r) {
+        System.out.println(" 绘制三角形 ");
+    }
 }
 
-//Shape�࣬����
+// Shape类，基类
 class Shape {
-	int m_type;
+    int m_type;
 }
 
 class Rectangle extends Shape {
-	Rectangle() {
-		super.m_type = 1;
-	}
+    Rectangle() {
+        super.m_type = 1;
+    }
 }
 
 class Circle extends Shape {
-	Circle() {
-		super.m_type = 2;
-	}
+    Circle() {
+        super.m_type = 2;
+    }
 }
 
-//������������
+// 新增画三角形
 class Triangle extends Shape {
-	Triangle() {
-		super.m_type = 3;
-	}
+    Triangle() {
+        super.m_type = 3;
+    }
 }
