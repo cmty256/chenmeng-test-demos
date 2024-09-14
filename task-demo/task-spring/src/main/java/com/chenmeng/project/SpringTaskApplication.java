ef54987d4da09d7688be65467743659f3251eae2
chenmeng <2691357857@qq.com>
2024-09-14 15:06:28 +0800

    SpringTask用法测试

---
+++ task-demo/task-spring/src/main/java/com/chenmeng/project/SpringTaskApplication.java
@@ -0,0 +1,16 @@
+package com.chenmeng.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenmeng
+ */
+@SpringBootApplication
+public class SpringTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTaskApplication.class, args);
+    }
+
+}
