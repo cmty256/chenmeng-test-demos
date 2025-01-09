55c4118f3e25d7d4aca7b64c356c7c9864a2b25d
chenmeng <2691357857@qq.com>
2025-01-09 11:56:59 +0800

    feat(task): 添加 xxl-job 示例模块

--- demo8-task/task-xxl-job/src/main/java/com/chenmeng/project/job/DemoJob.java
+++ demo8-task/task-xxl-job/src/main/java/com/chenmeng/project/job/DemoJob.java
@@ -1,5 +1,6 @@
 package com.chenmeng.project.job;
 
+import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.IJobHandler;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import org.slf4j.Logger;
@@ -18,9 +19,18 @@ public class DemoJob {
 
     private final AtomicInteger counts = new AtomicInteger();
 
+    /**
+     * BEAN 模式（方法形式）
+     * 注解配置：
+     * - 为Job方法添加注解 "@XxlJob(value="自定义jobhandler名称", init = "JobHandler初始化方法", destroy = "JobHandler销毁方法")"，
+     * - 注解value值对应的是调度中心新建任务的JobHandler属性的值。
+     */
     @XxlJob("demoJob")
     public void execute() throws Exception {
-        // 打印日志
+        // 业务打印日志，不能在调度中心查看
         logger.info("[execute][定时第 ({}) 次执行]", counts.incrementAndGet());
+
+        // 官方打印日志，可在调度中心查看
+        XxlJobHelper.log("[demoJob][定时第 ({}) 次执行]", counts.incrementAndGet());
     }
 }
