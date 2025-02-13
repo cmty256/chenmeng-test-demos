7f93c0f7723aa7f5b5181d97b8ad670ff4488d87
chenmeng <2691357857@qq.com>
2025-02-13 11:49:43 +0800

    feat(spring-security-oauth): 建立授权服务器

---
+++ demo14-spring-security-oauth/demo02-authorization-server-with-resource-owner-password-credentials/src/main/java/com/chenmeng/project/config/OAuth2AuthorizationServerConfig.java
@@ -0,0 +1,55 @@
+package com.chenmeng.project.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+/**
+ * 授权服务器配置
+ *
+ * @author chenmeng
+ */
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    /**
+     * 用户认证 Manager
+     */
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        oauthServer.checkTokenAccess("isAuthenticated()");
+//        oauthServer.tokenKeyAccess("isAuthenticated()")
+//                .checkTokenAccess("isAuthenticated()");
+//        oauthServer.tokenKeyAccess("permitAll()")
+//                .checkTokenAccess("permitAll()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                // Client 账号、密码。
+                .withClient("clientapp").secret("112233")
+                // 密码模式
+                .authorizedGrantTypes("password")
+                // 可授权的 Scope
+                .scopes("read_userinfo", "read_contacts")
+                // 可以继续配置新的 Client
+                // .and().withClient()
+        ;
+    }
+
+}
