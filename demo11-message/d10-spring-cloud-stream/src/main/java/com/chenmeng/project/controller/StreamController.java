fe258e9512a828eeed0d3de5a9883d1f776f06c0
chenmeng <2691357857@qq.com>
2025-02-26 15:00:47 +0800

    feat(message): 添加 spring cloud stream + kafka 示例，死信队列测试

--- demo11-message/d10-spring-cloud-stream/src/main/java/com/chenmeng/project/controller/StreamController.java
+++ demo11-message/d10-spring-cloud-stream/src/main/java/com/chenmeng/project/controller/StreamController.java
@@ -30,18 +30,36 @@ public class StreamController {
         return "发送成功";
     }
 
+    /**
+     * 自定义消息发送
+     * localhost:port/send/face/hello
+     *
+     * @param msg 消息
+     */
     @GetMapping("/face/{msg}")
     public String sendFaceMsg(@PathVariable("msg") String msg){
         customSendService.sendFaceMsg(msg);
         return "发送成功-Face";
     }
 
+    /**
+     * 自定义请求体发送
+     * localhost:port/send/human
+     *
+     * @param dto 消息
+     */
     @PostMapping("/human")
     public String sendHumanMsg(@RequestBody HumanDTO dto){
         customSendService.sendHumanMsg(dto);
         return "发送成功-Human";
     }
 
+    /**
+     * 消费重试测试
+     * localhost:port/send/vehicle
+     *
+     * @param dto 消息
+     */
     @PostMapping("/vehicle")
     public String sendVehicleMsg(@RequestBody VehicleDTO dto){
         customSendService.sendVehicleMsg(dto);
--- demo11-message/d10-spring-cloud-stream/src/main/java/com/chenmeng/project/message/listener/CustomReceiveService.java
+++ demo11-message/d10-spring-cloud-stream/src/main/java/com/chenmeng/project/message/listener/CustomReceiveService.java
@@ -3,6 +3,7 @@ package com.chenmeng.project.message.listener;
 import com.chenmeng.project.message.channel.CustomSinkChannel;
 import com.chenmeng.project.message.dto.HumanDTO;
 import com.chenmeng.project.message.dto.VehicleDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -12,6 +13,7 @@ import org.springframework.messaging.handler.annotation.Payload;
  *
  * @author chenmeng
  */
+@Slf4j
 @EnableBinding(CustomSinkChannel.class)
 public class CustomReceiveService {
 
@@ -27,7 +29,8 @@ public class CustomReceiveService {
 
     @StreamListener(CustomSinkChannel.VEHICLE_INPUT)
     public void receiveVehicle(VehicleDTO payload) {
-        System.out.println("Vehicle payload = " + payload);
-        // throw new RuntimeException("异常测试 -- 重试机制");
+        log.info("Vehicle payload = {}", payload);
+        // 无论设置了多少次重试，最终都只会打印一次异常
+        throw new RuntimeException("异常测试 -- 重试机制");
     }
 }
