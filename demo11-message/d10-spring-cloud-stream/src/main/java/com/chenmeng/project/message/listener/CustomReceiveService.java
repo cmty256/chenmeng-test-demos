19fb93e76e5a429cfcb1ef8c40bece0af88d6740
chenmeng <2691357857@qq.com>
2025-02-27 09:56:13 +0800

    feat(message): 添加全局的异常处理配置，通过订阅全局错误 Channel

--- demo11-message/d10-spring-cloud-stream/src/main/java/com/chenmeng/project/message/listener/CustomReceiveService.java
+++ demo11-message/d10-spring-cloud-stream/src/main/java/com/chenmeng/project/message/listener/CustomReceiveService.java
@@ -6,7 +6,9 @@ import com.chenmeng.project.message.dto.VehicleDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.context.IntegrationContextUtils;
+import org.springframework.messaging.support.ErrorMessage;
 
 /**
  * 自定义通道接收业务类（消费者）
@@ -32,5 +34,33 @@ public class CustomReceiveService {
         log.info("Vehicle payload = {}", payload);
         // 无论设置了多少次重试，最终都只会打印一次异常
         throw new RuntimeException("异常测试 -- 重试机制");
+    }
+
+    // /**
+    //  * 写法测试无效，暂时不知道原因
+    //  * 局部的异常处理：通过订阅指定错误 Channel
+    //  *
+    //  * @param errorMessage
+    //  */
+    // @ServiceActivator(inputChannel = "error.vehicle_topic.vehicle_consumer_group")
+    // public void handleError(ErrorMessage errorMessage) {
+    //     log.error("[handleError][订阅到错误Channel：{}]", "error.vehicle_topic.vehicle_consumer_group");
+    //     log.error("[handleError][payload：{}]", errorMessage.getPayload().getMessage());
+    //     log.error("[handleError][originalMessage：{}]", errorMessage.getOriginalMessage());
+    //     log.error("[handleError][headers：{}]", errorMessage.getHeaders());
+    // }
+
+    /**
+     * 全局的异常处理：通过订阅全局错误 Channel
+     *
+     * @param errorMessage
+     */
+    @StreamListener(IntegrationContextUtils.ERROR_CHANNEL_BEAN_NAME) // errorChannel
+    public void globalHandleError(ErrorMessage errorMessage) {
+        log.error("[globalHandleError][payload：{}]", errorMessage.getPayload().getMessage());
+        log.error("[globalHandleError][originalMessage：{}]", errorMessage.getOriginalMessage());
+        log.error("[globalHandleError][headers：{}]", errorMessage.getHeaders());
+
+        // 可存储到数据库，后续处理
     }
 }
